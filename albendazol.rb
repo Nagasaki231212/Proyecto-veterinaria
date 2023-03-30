17e2c09cafebc88e910109c2dc4995ec566048be
Oscar Gil <oscargil19971212@gmail.com>
2023-03-30 19:37:51 -0400

    agregue el metodo a la clase albendazol

--- albendazol.rb
+++ albendazol.rb
@@ -1,4 +1,15 @@
 class Albendazol < Medicina
+    def calcular_cc_animal(animal)
+        case animal
+        when Gato
+            puts animal.edad * animal.peso / 200.0
+        when Perro
+            puts animal.edad * animal.peso / 100.0
+        when Toro 
+          nil
+        else
+          puts "Consulta" 
+    end
 end 
 
 
